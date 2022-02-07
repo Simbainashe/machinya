6e3d09ce4d350ef706c1eabb2747331d2544f68a
fact.musingarimi <fact.musingarimi@wonderlabz.com>
2022-02-07 09:50:55 +0200

    Commit message

---
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/CreditsServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.econetwireless.epay.business.services.impl;
+
+import junit.framework.TestCase;
+
+public class CreditsServiceImplTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+}
